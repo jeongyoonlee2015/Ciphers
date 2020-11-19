5bbaf784c1caed63322ad9b58a375e79b24c8db7
jeongyoonlee2015 <happyjoy2005@gmail.com>
2020-11-19 22:02:01 +0900

    ADD extEuclidean

---
+++ extendedEuclidean.py
@@ -0,0 +1,13 @@
+def extended_Euclidean(a, b):
+    (old_r, r) = (a, b)
+    (old_s, s) = (1, 0)
+    (old_t, t) = (0, 1)
+
+    while r != 0:
+        quotient = old_r // r
+        (old_r, r) = (r, old_r - quotient * r)
+        (old_s, s) = (s, old_s - quotient * s)
+        (old_t, t) = (t, old_t - quotient * t)
+
+    print("(s, t = (%d. %d)" %(old_s, old_t))
+    print("gcd(a, b) = %d" % old_r)
