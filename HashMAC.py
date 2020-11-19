77677399273987c782cdcaf08c8a80731648e902
jeongyoonlee2015 <happyjoy2005@gmail.com>
2020-11-20 00:45:45 +0900

    Modifying Hash

--- HashMAC.py
+++ HashMAC.py
@@ -1,23 +1,18 @@
 # #m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
-#
-# def Encoding(m):#문자열 인코딩
-#     m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
-#     M = []
-#     test = []
-#     for i in range(len(m)):
-#        M.append(ord(m[i]))
-#     return M
 
-#압축함수구현: Davies-Meyer
-x = 0xa5c8f1ee #x = hex(2781409774)
-def CompressionFunction(x, M):
-    y = BlockCipherEncrypt(x, M) ^ M
-    return y
+def Encoding(m):#문자열 인코딩
+    m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
+    M = []
+    test = []
+    for i in range(len(m)):
+        M.append(ord(m[i]))
+    return M
 
 # From my Professor
-P = 0x12345678 #P = hex(305419896)
-K = 0xC58FA10B #K = hex(3314524427)
+
 def BlockCipherEncrypt(K, P):
+    P = 0x12345678  # P = hex(305419896)
+    K = 0xC58FA10B  # K = hex(3314524427)
     S = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7]
     T = K ^ P
     X = [0 for j in range(8)]
@@ -38,57 +33,72 @@ def BlockCipherEncrypt(K, P):
         Z[7] = Y[0] ^ Y[3] ^ Y[4] ^ Y[5] ^ Y[6]
 
         for j in range(8): T = (T << 4)| Z[7 - j]
-        T = (T&0xffffffff) ^ K
+        T = (T & 0xffffffff) ^ K
     return T
 
+#압축함수구현: Davies-Meyer
+
+def CompressionFunction(T, M):
+    # x = 0xa5c8f1ee  # x = hex(2781409774)
+    y = BlockCipherEncrypt(T, M)
+    z = y ^ M
+    # print("y is: ", y)
+    print("y is:\n", CompressionFunction(T, M))
+    print("ys is ", y)
+    # return y
+    return z
 
 
-# #해시함수
-# def HashFunction(M):
-#     #MD-Strengthening Padding
-#     Mblock = []#메시지 블록 배열
-#     Mlen = len(M) #메시지 길이(바이트 수) = 16비트 값
-#     if Mlen % 4 == 0:
-#         for i in range(Mlen // 4):
-#             temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16)|(M[4 * i + 2] << 8) | M[4 * i + 3]
-#             Mblock.append(temp)
-#         temp = (0x80000000) | Mlen
-#         Mblock.append(temp)
-#
-#     elif Mlen % 4 == 1:
-#         for i in range (Mlen // 4):
-#             temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16) | (M[4 * i + 2] << 8) | M[4 * i + 3]
-#             Mblock.append(temp)
-#         temp = (M[Mlen - 1] << 24) | (0x800000) | Mlen
-#         Mblock.append(temp)
-#
-#     elif Mlen % 4 == 2:
-#         for i in range (Mlen // 4):
-#             temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16) | (M[4 * i + 2] << 8) | M[4 * i + 3]
-#             Mblock.append(temp)
-#         temp = (M[Mlen - 2] << 24) | (M[Mlen - 1] << 16) | (0x8000) | Mlen
-#         Mblock.append(temp)
-#         Mblock.append(Mlen)
-#
-#     else: #Mlen % 4 == 3:
-#         for i in range (Mlen // 4):
-#             temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16) | (M[4 * i + 2] << 8) | M[4 * i + 3]
-#             Mblock.append(temp)
-#         temp = (M[Mlen - 3] << 24) | (M[Mlen - 2] << 16) | (M[Mlen - 8] << 16) | (0x8000) | Mlen
-#         Mblock.append(temp)
-#         Mblock.append(Mlen)
-#
-#     X = 0xa5c8f1ee
-#     for i in range(Mlen):
-#         X = CompressionFunction(X, Mblock[i])
-#     return X
-#
-# #해시함수 실행
+
+#해시함수
+def HashFunction(M):
+    X = 0xa5c8f1ee
+    #MD-Strengthening Padding
+    Mblock = []#메시지 블록 배열
+    Mlen = len(M) #메시지 길이(바이트 수) = 16비트 값
+    Mblen = 0#메시지 블록
+
+    if Mlen % 4 == 0:
+        for i in range(Mlen // 4):
+            temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16)|(M[4 * i + 2] << 8) | M[4 * i + 3]
+            Mblock.append(temp)
+        temp = (0x80000000) | Mlen
+        Mblock.append(temp)
+
+    elif Mlen % 4 == 1:
+        for i in range (Mlen // 4):
+            temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16) | (M[4 * i + 2] << 8) | M[4 * i + 3]
+            Mblock.append(temp)
+        temp = (M[Mlen - 1] << 24) | (0x800000) | Mlen
+        Mblock.append(temp)
+
+    elif Mlen % 4 == 2:
+        for i in range (Mlen // 4):
+            temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16) | (M[4 * i + 2] << 8) | M[4 * i + 3]
+            Mblock.append(temp)
+        temp = (M[Mlen - 2] << 24) | (M[Mlen - 1] << 16) | (0x8000)
+        Mblock.append(temp)
+        Mblock.append(Mlen)
+
+    else: #Mlen % 4 == 3:
+        for i in range (Mlen // 4):
+            temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16) | (M[4 * i + 2] << 8) | M[4 * i + 3]
+            Mblock.append(temp)
+        temp = (M[Mlen - 3] << 24) | (M[Mlen - 2] << 16) | (M[Mlen - 1] << 8) | (0x80)
+        Mblock.append(temp)
+        Mblock.append(Mlen)
+
+    for i in range(Mblen):
+        X = CompressionFunction(X, Mblock[i])
+    print("modi X is", hex(X))
+    return X
+
+#해시함수 실행
 # m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
 # M = Encoding(m)
 # print(hex(HashFunction(M)))
-# #결과: 0x3e649af1
-#
+#결과: 0x3e649af1
+
 # # #HMAC구현
 # # def HMAC(K, M):
 # #     K1 = K ^ 0xabcdeff
