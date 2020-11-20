94da0b91442734b5fa4d3ecec8c3bb0b2b6fc7e1
jeongyoonlee2015 <happyjoy2005@gmail.com>
2020-11-20 12:40:51 +0900

    Modifying

--- testAS7.py
+++ testAS7.py
@@ -1,15 +1,14 @@
-# #m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
-
+m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
 def Encoding(m):#문자열 인코딩
-    m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
-    M0 = []
+
+    M = []
     for i in range(len(m)):
-        M0.append(ord(m[i]))
-    return M0
+        M.append(ord(m[i]))
+    return M
 
-# From my Professor
+#X = 0xa5c8f1ee
 
-def BlockCipherEncrypt(K, P):
+def BlockCipherEncrypt(K, P):# 키, 평문블록
     P = 0x12345678  # P = hex(305419896)
     K = 0xC58FA10B  # K = hex(3314524427)
     S = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7]
@@ -36,19 +35,14 @@ def BlockCipherEncrypt(K, P):
     return T
 
 #압축함수구현: Davies-Meyer
-def CompressionFunction(T, M0):
-    # x = 0xa5c8f1ee  # x = hex(2781409774)
-    y = BlockCipherEncrypt(T, M0)
-    M = y ^ M0
-    print("y is:\n", CompressionFunction(T, M0))
-    print("z is ", M)
-    return M
+# x = 0xa5c8f1ee  # x = hex(2781409774)
 
-
+def CompressionFunction(X, M):#둘다 32bit
+    M1 = BlockCipherEncrypt(X, M) ^ M
+    return M1
 
 #해시함수
 def HashFunction(M):
-    X = 0xa5c8f1ee
     #MD-Strengthening Padding
     Mblock = []#메시지 블록 배열
     Mlen = len(M) #메시지 길이(바이트 수) = 16비트 값
@@ -56,7 +50,7 @@ def HashFunction(M):
 
     if Mlen % 4 == 0:
         for i in range(Mlen // 4):
-            temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16)|(M[4 * i + 2] << 8) | M[4 * i + 3]
+            temp = (M[4 * i] << 24) | (M[4 * i + 1] << 16) | (M[4 * i + 2] << 8) | M[4 * i + 3]
             Mblock.append(temp)
         temp = (0x80000000) | Mlen
         Mblock.append(temp)
@@ -84,33 +78,19 @@ def HashFunction(M):
         Mblock.append(temp)
         Mblock.append(Mlen)
 
-    for i in range(Mblen):
-        X = CompressionFunction(X, Mblock[i])
-    print("modi X is", hex(X))
-    return X
+    # X = 0xa5c8f1ee
 
-#해시함수 실행
-# m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
+    X = 0xa5c8f1ee
+    Y = CompressionFunction(X, Mblock[i])
+    j = i + 1
+    for j in range(Mblen):
+        Y = CompressionFunction(Y, Mblock[j])
+        return Y
+    print("out Y:", Y)
+    return Y
+
+# from testAS7 import *
 # M = Encoding(m)
 # print(hex(HashFunction(M)))
-#결과: 0x3e649af1
-
-# # #HMAC구현
-# # def HMAC(K, M):
-# #     K1 = K ^ 0xabcdeff
-# #     K2 = K ^ 0x12345678
-# #     M1 = []
-# #     for i in range(4): M1.append((K1 >> (24 - 8 * i)) & 0xff)
-# #     for i in range(len(M)): M1.append(M[i])
-# #     Y = HashFunction(M1)
-# #     M2 = []
-# #     for i in range(4): M2.append((K2 >> (24 - 8 * i)) & 0xff)
-# #     for i in range(4): M2.append((Y >> (24 - 8 * i )) & 0xff)
-# #     Y = HashFunction(M2)
-# #     return Y
-# # # HMAC 실행
-# # m = "Twinkle"
-# # M = Encoding(m)
-# # Key = 0xcf3659ea
-# # print(hex(HMAC(Key, M)))
-# # #결과: 0xf24837f9
+# out Y: 470704440
+# 0x1c0e6138
--- testinit.py
+++ testinit.py
@@ -1,10 +1,10 @@
 from HashMAC import *
 m = "Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle twinkle little star How I wonder what you are"
-M = Encoding(m)
+M0 = Encoding(m)
 P = 0x12345678  # P = hex(305419896)
 K = 0xC58FA10B  # K = hex(3314524427)
 T = BlockCipherEncrypt(K, P)
 
 print("T:", hex(T))
-print("Encoding m is:\n", M)
-print(hex(HashFunction(M)))
+print("Encoding m is:\n", M0)
+print(hex(HashFunction(M0)))
