a919b6fc8101dfd11bbd639993045b626edf3653
jeongyoonlee2015 <happyjoy2005@gmail.com>
2020-11-28 01:23:08 +0900

    Recover Hill-Cipher

--- Theoretical/hillCipher.py
+++ Theoretical/hillCipher.py
@@ -2,10 +2,8 @@ import numpy as np
 from sympy import Matrix
 import string
 import random
-dim = 2
-alphabet = string.ascii_uppercase
-#key = np.matrix([[1, 2], [2, 5]])
-#HOTBYJWOGQIH
+dim = 2 #n차원 행렬
+cipher = string.ascii_uppercase
 def main():
     mode = input("Select Encrypt or Decrypt:")
     if mode == 'Encrypt':
@@ -18,19 +16,19 @@ def encrypt():
     plaintext = input("Input your plaintext for encryption:")
     encryption = ""
     for index, i in enumerate(plaintext):
-        values = []
+        value = []
         if index % dim == 0:
-            for j in range(dim):
+            for j in range(0, dim):
                 if (index + j < len(plaintext)):
-                    values.append([alphabet.index(plaintext[index + j])])
+                    value.append([cipher.index(plaintext[index + j])])
                 else:
-                    values.append([random.randint(0, 25)])
+                    value.append([random.randint(0, 25)])
 
-            vector = np.matrix(values)
+            vector = np.matrix(value)
             vector = key * vector
             vector %= 26
-            for j in range(dim):
-                encryption += alphabet[vector.item(j)]
+            for j in range(0, dim):
+                encryption += cipher[vector.item(j)]
     print(encryption)
 
 def decrypt():
@@ -42,18 +40,18 @@ def decrypt():
     key = key.tolist()
 
     for index, i in enumerate(ciphertext):
-        values = []
+        value = []
 
         if index % dim == 0:
             for j in range(0, dim):
-                values.append([alphabet.index(ciphertext[index + j])])
+                value.append([cipher.index(ciphertext[index + j])])
 
-            vector = np.matrix(values)
+            vector = np.matrix(value)
             vector = key * vector
             vector %= 26
 
             for j in range(0, dim):
-                decryption += alphabet[vector.item(j)]
+                decryption += cipher[vector.item(j)]
     print(decryption)
 
 if __name__ == '__main__':
